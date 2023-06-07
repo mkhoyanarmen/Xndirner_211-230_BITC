9f7e10e33a3e6e695ac5f370777774f8097e49c1
mkhoyanarmen <93216230+mkhoyanarmen@users.noreply.github.com>
2023-06-07 19:38:31 +0400

    Add project files.

---
+++ Xndirner_211-230/Program.cs
@@ -0,0 +1,598 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xndirner_211_230
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //211
+            //Random rnd = new Random();
+            //int n;
+            //do
+            //{
+            //    Console.WriteLine("Mutqagrel zangvaci chapy");
+            //    n = int.Parse(Console.ReadLine());
+            //} while (!(n > 0));
+
+            //int[] arr = new int[n];
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    arr[i] = rnd.Next(-10, 20);
+            //    Console.Write(arr[i] + " ");
+            //}
+            //Console.WriteLine();
+
+            //int sum = 0, count = 0;
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    if (arr[i] > 0)
+            //    {
+            //        sum += arr[i];
+            //        count++;
+            //    }
+            //}
+            //Console.WriteLine("drakan tarreri mijin tvabanakan: " + (double)sum / count);
+
+            //212
+            //Random rnd = new Random();
+            //int n;
+            //do
+            //{
+            //    Console.WriteLine("Mutqagrel zangvaci chapy");
+            //    n = int.Parse(Console.ReadLine());
+            //} while (!(n > 0));
+
+            //int[] arr = new int[n];
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    arr[i] = rnd.Next(-10, 20);
+            //    Console.Write(arr[i] + " ");
+            //}
+            //Console.WriteLine();
+
+            //int sum = 0, count = 0;
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    if (arr[i] > 0)
+            //    {
+            //        sum += arr[i] * arr[i];
+            //        count++;
+            //    }
+            //}
+            //Console.WriteLine("drakan tarreri mijin qarakusayin: " + Math.Sqrt(sum / count));
+
+            //213
+            //Random rnd = new Random();
+            //int n;
+            //do
+            //{
+            //    Console.WriteLine("Mutqagrel zangvaci chapy");
+            //    n = int.Parse(Console.ReadLine());
+            //} while (!(n > 0));
+
+            //int[] arr = new int[n];
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    arr[i] = rnd.Next(-10, 20);
+            //    Console.Write(arr[i] + " ");
+            //}
+            //Console.WriteLine();
+
+            //int sum = 0, count = 0;
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    if (arr[i] < 0)
+            //    {
+            //        sum += arr[i] * arr[i];
+            //        count++;
+            //    }
+            //}
+            //Console.WriteLine("bacasakan tarreri mijin qarakusayin: " + Math.Sqrt(sum / count));
+
+            //214
+            //Random rnd = new Random();
+            //int n;
+            //do
+            //{
+            //    Console.WriteLine("Mutqagrel zangvaci chapy");
+            //    n = int.Parse(Console.ReadLine());
+            //} while (!(n > 0));
+
+            //int[] arr = new int[n];
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    arr[i] = rnd.Next(-10, 20);
+            //    Console.Write(arr[i] + " ");
+            //}
+            //Console.WriteLine();
+
+            //int sum = 0, count = 0;
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    if (arr[i] < 0)
+            //    {
+            //        sum += arr[i];
+            //        count++;
+            //    }
+            //}
+            //Console.WriteLine("bacasakan tarreri mijin tvabanakany: " + sum / count);
+
+            //215
+            //Random rnd = new Random();
+            //int n;
+            //do
+            //{
+            //    Console.WriteLine("Mutqagrel zangvaci chapy");
+            //    n = int.Parse(Console.ReadLine());
+            //} while (!(n > 0));
+
+            //int[] arr = new int[n];
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    arr[i] = rnd.Next(-10, 20);
+            //    Console.Write(arr[i] + " ");
+            //}
+            //Console.WriteLine();
+
+            //int sum = 0;
+            //for (int i = 0; i < arr.Length; i+=2)
+            //{
+            //    sum += arr[i];
+            //}
+            //Console.WriteLine("zuyg index unecox tarreri gumary: " + sum);
+
+            //216
+            //Random rnd = new Random();
+            //int n;
+            //do
+            //{
+            //    Console.WriteLine("Mutqagrel zangvaci chapy");
+            //    n = int.Parse(Console.ReadLine());
+            //} while (!(n > 0));
+
+            //int[] arr = new int[n];
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    arr[i] = rnd.Next(-10, 20);
+            //    Console.Write(arr[i] + " ");
+            //}
+            //Console.WriteLine();
+
+            //int p = 1;
+            //for (int i = 0; i < arr.Length; i += 2)
+            //{
+            //    p *= arr[i];
+            //}
+            //Console.WriteLine("zuyg index unecox tarreri artadryaly: " + p);
+
+            //217
+            //Random rnd = new Random();
+            //int n;
+            //do
+            //{
+            //    Console.WriteLine("Mutqagrel zangvaci chapy");
+            //    n = int.Parse(Console.ReadLine());
+            //} while (!(n > 0));
+
+            //int[] arr = new int[n];
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    arr[i] = rnd.Next(-10, 20);
+            //    Console.Write(arr[i] + " ");
+            //}
+            //Console.WriteLine();
+
+            //int p = 1;
+            //for (int i = 1; i < arr.Length; i += 2)
+            //{
+            //    p *= arr[i] * arr[i];
+            //}
+            //Console.WriteLine("kent index unecox tarreri qarakusineri artadryaly: " + p);
+
+            //218
+            //Random rnd = new Random();
+            //int n;
+            //do
+            //{
+            //    Console.WriteLine("Mutqagrel zangvaci chapy");
+            //    n = int.Parse(Console.ReadLine());
+            //} while (!(n > 0));
+
+            //int[] arr = new int[n];
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    arr[i] = rnd.Next(-10, 20);
+            //    Console.Write(arr[i] + " ");
+            //}
+            //Console.WriteLine();
+
+            //int sum = 0;
+            //for (int i = 1; i < arr.Length; i += 2)
+            //{
+            //    sum += Math.Abs(arr[i]);
+            //}
+            //Console.WriteLine("kent index unecox tarreri bacardzak arjeqneri gumary: " + sum);
+
+            //219
+            //Random rnd = new Random();
+            //Console.WriteLine("Mutqagrel k tivy");
+            //int k = int.Parse(Console.ReadLine());
+            //int n;
+            //do
+            //{
+            //    Console.WriteLine("Mutqagrel zangvaci chapy");
+            //    n = int.Parse(Console.ReadLine());
+            //} while (!(n > 0));
+
+            //int[] arr = new int[n];
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    arr[i] = rnd.Next(-10, 20);
+            //    Console.Write(arr[i] + " ");
+            //}
+            //Console.WriteLine();
+
+            //int count = 0;
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    if (i % k == 0)
+            //    {
+            //        count++;
+            //    }
+            //}
+            //Console.WriteLine(count);
+
+            //220
+            //Random rnd = new Random();
+            //int n;
+            //do
+            //{
+            //    Console.WriteLine("Mutqagrel zangvaci chapy");
+            //    n = int.Parse(Console.ReadLine());
+            //} while (!(n > 0));
+
+            //int[] arr = new int[n];
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    arr[i] = rnd.Next(-10, 20);
+            //    Console.Write(arr[i] + " ");
+            //}
+            //Console.WriteLine();
+
+            //int countDrakan = 0, countBacasakan = 0;
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    if (arr[i] > 0)
+            //    {
+            //        countDrakan++;
+            //    }else
+            //        countBacasakan++;
+            //}
+            //Console.WriteLine($"Drakan tarreri qanak: {countDrakan}\n" +
+            //    $"Bacasakan tarreri qanak: {countBacasakan}");
+
+            //221
+            //Random rnd = new Random();
+            //int n, a, b;
+            //do
+            //{
+            //    Console.WriteLine("Mutqagrel a");
+            //    a = int.Parse(Console.ReadLine());
+            //    Console.WriteLine("Mutqagrel b");
+            //    b = int.Parse(Console.ReadLine());
+            //} while (!(a < b));
+
+            //do
+            //{
+            //    Console.WriteLine("Mutqagrel zangvaci chapy");
+            //    n = int.Parse(Console.ReadLine());
+            //} while (!(n > 0));
+
+            //int[] arr = new int[n];
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    arr[i] = rnd.Next(-10, 20);
+            //    Console.Write(arr[i] + " ");
+            //}
+            //Console.WriteLine();
+
+            //int sum = 0;
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    if (arr[i] <= b && arr[i] >= a)
+            //    {
+            //        sum += arr[i];
+            //    }
+            //}
+            //Console.WriteLine("[a; b] mijakayqin patkanox tarreri gumary: " + sum);
+
+            //222
+            //Random rnd = new Random();
+            //int n, c, d;
+            //do
+            //{
+            //    Console.WriteLine("Mutqagrel c");
+            //    c = int.Parse(Console.ReadLine());
+            //    Console.WriteLine("Mutqagrel d");
+            //    d = int.Parse(Console.ReadLine());
+            //} while (!(c < d));
+
+            //do
+            //{
+            //    Console.WriteLine("Mutqagrel zangvaci chapy");
+            //    n = int.Parse(Console.ReadLine());
+            //} while (!(n > 0));
+
+            //int[] arr = new int[n];
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    arr[i] = rnd.Next(-10, 20);
+            //    Console.Write(arr[i] + " ");
+            //}
+            //Console.WriteLine();
+
+            //int p = 1;
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    if (arr[i] < d && arr[i] > c)
+            //    {
+            //        p *= arr[i];
+            //    }
+            //}
+            //Console.WriteLine("(c; d) mijakayqin patkanox tarreri artadryaly: " + p);
+
+            //223
+            //Random rnd = new Random();
+            //int n, a, b;
+            //do
+            //{
+            //    Console.WriteLine("Mutqagrel a");
+            //    a = int.Parse(Console.ReadLine());
+            //    Console.WriteLine("Mutqagrel b");
+            //    b = int.Parse(Console.ReadLine());
+            //} while (!(a < b));
+
+            //do
+            //{
+            //    Console.WriteLine("Mutqagrel zangvaci chapy");
+            //    n = int.Parse(Console.ReadLine());
+            //} while (!(n > 0));
+
+            //int[] arr = new int[n];
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    arr[i] = rnd.Next(-10, 20);
+            //    Console.Write(arr[i] + " ");
+            //}
+            //Console.WriteLine();
+
+            //int count = 0;
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    if (arr[i] < b && arr[i] > a)
+            //    {
+            //        count++;
+            //    }
+            //}
+            //Console.WriteLine("[a; b] mijakayqin patkanox tarreri qanaky: " + count);
+
+            //224
+            //Random rnd = new Random();
+            //int n;
+
+            //Console.WriteLine("Mutqagrel k tivy");
+            //int k = int.Parse(Console.ReadLine());
+
+            //do
+            //{
+            //    Console.WriteLine("Mutqagrel zangvaci chapy");
+            //    n = int.Parse(Console.ReadLine());
+            //} while (!(n > 0));
+
+            //int[] arr = new int[n];
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    arr[i] = rnd.Next(-10, 20);
+            //    Console.Write(arr[i] + " ");
+            //}
+            //Console.WriteLine();
+
+            //double sum = 0;
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    if (Math.Abs(arr[i]) < k)
+            //    {
+            //        sum += Math.Pow(arr[i], 3);
+            //    }
+            //}
+            //Console.WriteLine(sum);
+
+            //225
+            //Random rnd = new Random();
+            //int n;
+
+            //Console.WriteLine("Mutqagrel t tivy");
+            //int t = int.Parse(Console.ReadLine());
+
+            //do
+            //{
+            //    Console.WriteLine("Mutqagrel zangvaci chapy");
+            //    n = int.Parse(Console.ReadLine());
+            //} while (!(n > 0));
+
+            //int[] arr = new int[n];
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    arr[i] = rnd.Next(-10, 20);
+            //    Console.Write(arr[i] + " ");
+            //}
+            //Console.WriteLine();
+
+            //double p = 1;
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    if (Math.Abs(arr[i]) < t)
+            //    {
+            //        p *= arr[i];
+            //    }
+            //}
+            //Console.WriteLine(p);
+
+            //226
+            //Random rnd = new Random();
+            //int n;
+
+            //Console.WriteLine("Mutqagrel k tivy");
+            //int k = int.Parse(Console.ReadLine());
+
+            //do
+            //{
+            //    Console.WriteLine("Mutqagrel zangvaci chapy");
+            //    n = int.Parse(Console.ReadLine());
+            //} while (!(n > 0));
+
+            //int[] arr = new int[n];
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    arr[i] = rnd.Next(-10, 20);
+            //    Console.Write(arr[i] + " ");
+            //}
+            //Console.WriteLine();
+
+            //int count = 0;
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    if (Math.Abs(arr[i]) < k)
+            //    {
+            //        count++;
+            //    }
+            //}
+            //Console.WriteLine(count);
+
+            //227
+            //Random rnd = new Random();
+            //int n;
+
+            //Console.WriteLine("Mutqagrel k tivy");
+            //int k = int.Parse(Console.ReadLine());
+
+            //do
+            //{
+            //    Console.WriteLine("Mutqagrel zangvaci chapy");
+            //    n = int.Parse(Console.ReadLine());
+            //} while (!(n > 0));
+
+            //int[] arr = new int[n];
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    arr[i] = rnd.Next(-10, 20);
+            //    Console.Write(arr[i] + " ");
+            //}
+            //Console.WriteLine();
+
+            //int sum = 0, count = 0;
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    if (i % k == 0)
+            //    {
+            //        sum += arr[i];
+            //        count++;
+            //    }
+            //}
+            //Console.WriteLine((double)sum/count);
+
+            //228
+            //Random rnd = new Random();
+            //int n;
+
+            //Console.WriteLine("Mutqagrel k tivy");
+            //int k = int.Parse(Console.ReadLine());
+
+            //do
+            //{
+            //    Console.WriteLine("Mutqagrel zangvaci chapy");
+            //    n = int.Parse(Console.ReadLine());
+            //} while (!(n > 0));
+
+            //int[] arr = new int[n];
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    arr[i] = rnd.Next(-10, 20);
+            //    Console.Write(arr[i] + " ");
+            //}
+            //Console.WriteLine();
+
+            //int sum = 0;
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    if (i % k == 0)
+            //    {
+            //        sum += arr[i];
+            //    }
+            //}
+            //Console.WriteLine(sum);
+
+            //229
+            //Random rnd = new Random();
+            //int n;
+
+            //do
+            //{
+            //    Console.WriteLine("Mutqagrel zangvaci chapy");
+            //    n = int.Parse(Console.ReadLine());
+            //} while (!(n > 0));
+
+            //int[] arr = new int[n];
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    arr[i] = rnd.Next(-10, 20);
+            //    Console.Write(arr[i] + " ");
+            //}
+            //Console.WriteLine();
+
+            //int p = 1;
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    if (arr[i] - i > 0)
+            //    {
+            //        p *= arr[i];
+            //    }
+            //}
+            //Console.WriteLine(p);
+
+            //230
+            Random rnd = new Random();
+            int n;
+
+            Console.WriteLine("Mutqagrel k tivy");
+            int k = int.Parse(Console.ReadLine());
+
+            do
+            {
+                Console.WriteLine("Mutqagrel zangvaci chapy");
+                n = int.Parse(Console.ReadLine());
+            } while (!(n > 0));
+
+            int[] arr = new int[n];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rnd.Next(-10, 20);
+                Console.Write(arr[i] + " ");
+            }
+            Console.WriteLine();
+
+            int sum = 0, count = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum += arr[i] * arr[i];
+                count++;
+            }
+            Console.WriteLine("Mijin qarakusayin: " + (double)sum / count);
+        }
+    }
+}
